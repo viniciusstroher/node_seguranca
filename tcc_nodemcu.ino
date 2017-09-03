f548a498baaaf5971647acebe3fb45808ba1f020
vini <vini@gmail.com>
2017-09-03 16:26:53 -0300

    fix

--- tcc_nodemcu.ino
+++ tcc_nodemcu.ino
@@ -1,6 +1,6 @@
 #include <EEPROM.h>
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>  
+#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
 //EEPROM
@@ -16,7 +16,7 @@ WiFiClient client;
 //CONTROLADOR
 void Controlador_modoAdmin();
 void Controlador_resetaControlador();
-void Controlador_enviaDadosServer(String ip,String porta,String hook,String data);
+void Controlador_enviaDadosServer(String ip, String porta, String hook, String data);
 
 
 //VARIAVEIS
@@ -48,110 +48,110 @@ void setup() {
   Serial.begin(115200);
 
   fazerReset();
-    
-  operacao        = EEPROM_getValueEEPROM(1); 
+
+  operacao        = EEPROM_getValueEEPROM(1);
   delay(10);
-  Serial.println("Operacao: "+operacao);
- 
-  if(operacao.equals("A")){
+  Serial.println("Operacao: " + operacao);
+
+  if (operacao.equals("A")) {
     Serial.println("MODO ADMIN ATIVADO");
-    
+
     //CONFIGURA MODO ADMIN
     Controlador_modoAdmin();
 
   }
-  
-  if(operacao.equals("O")){
+
+  if (operacao.equals("O")) {
     Serial.println("MODO OPERACAO ATIVADO");
-    ssid     = EEPROM_getValueEEPROM(2); 
+    ssid     = EEPROM_getValueEEPROM(2);
     delay(10);
-    pass     = EEPROM_getValueEEPROM(3); 
+    pass     = EEPROM_getValueEEPROM(3);
     delay(10);
-    ip       = EEPROM_getValueEEPROM(4);  
+    ip       = EEPROM_getValueEEPROM(4);
     delay(10);
-    porta    = EEPROM_getValueEEPROM(5);  
+    porta    = EEPROM_getValueEEPROM(5);
     delay(10);
-    senhaApi = EEPROM_getValueEEPROM(6);  
+    senhaApi = EEPROM_getValueEEPROM(6);
     delay(10);
-    Serial.println("SSID: "+ssid+" Pass: "+pass+" IP:"+ip+" Porta: "+porta+" Senha api: "+senhaApi);
+    Serial.println("SSID: " + ssid + " Pass: " + pass + " IP:" + ip + " Porta: " + porta + " Senha api: " + senhaApi);
 
     //CONFIGURA MODO DE OPERACAO
-    Controlador_modoOperacao(ssid,pass,ip,porta);
-    
+    Controlador_modoOperacao(ssid, pass, ip, porta);
+
     //SETAR PINAGEM DE SENSORES AQUI
     pinMode(PINO_SENSOR_MAGNETICO, INPUT_PULLUP);
     pinMode(PINO_SENSOR_PIR, INPUT_PULLUP);
-    digitalWrite(PINO_SENSOR_MAGNETICO,HIGH);
-    digitalWrite(PINO_SENSOR_PIR,HIGH);
-    
+    digitalWrite(PINO_SENSOR_MAGNETICO, HIGH);
+    digitalWrite(PINO_SENSOR_PIR, HIGH);
+
     estadoPorta = digitalRead(PINO_SENSOR_MAGNETICO);
   }
 
 }
- 
+
 void loop() {
   fazerReset();
-  
-  if(operacao.equals("A")){
+
+  if (operacao.equals("A")) {
     server.handleClient();
   }
 
-  if(operacao.equals("O")){
+  if (operacao.equals("O")) {
     capturaSensores();
   }
 
   wdt_reset();
 }
 
-void capturaSensores(){
+void capturaSensores() {
   int estadoSensorMagnetico = digitalRead(PINO_SENSOR_MAGNETICO);
   int estadoSensorPir       = digitalRead(PINO_SENSOR_PIR);
-    
-  Serial.println("Porta aberta : "+String(estadoSensorMagnetico));
-  Serial.println("Movimentacao : "+String(estadoSensorPir));
 
-    
+  Serial.println("Porta aberta : " + String(estadoSensorMagnetico));
+  Serial.println("Movimentacao : " + String(estadoSensorPir));
+
+  Serial.println("Idle 200: " + String(enviar_estado));
   //VERIFICA SE O SENSOR DA PORTA ESTA ABERTO
   //NO MEU CASO O zero significa o circuito aberto
-  //if(estadoSensorMagnetico == 1 || estadoSensorPir == 1){  
-  if((estadoPorta != estadoSensorMagnetico) || estadoSensorPir == 1){  
-    if(estadoPorta != estadoSensorMagnetico){
-       estadoPorta = estadoSensorMagnetico;
-       if(estadoPorta == 1){
-          Controlador_enviaDadosServer(nomeControlador,ip,porta,senhaApi,"/porta_aberta","{\"magnetico\":true}");  
-       }else{
-          Controlador_enviaDadosServer(nomeControlador,ip,porta,senhaApi,"/porta_aberta","{\"magnetico\":false}");  
-       }
-      
+  //if(estadoSensorMagnetico == 1 || estadoSensorPir == 1){
+  if ((estadoPorta != estadoSensorMagnetico) || estadoSensorPir == 1) {
+    if (estadoPorta != estadoSensorMagnetico) {
+      estadoPorta = estadoSensorMagnetico;
+      if (estadoPorta == 1) {
+        Controlador_enviaDadosServer(nomeControlador, ip, porta, senhaApi, "/porta_aberta", "{\"magnetico\":true}");
+      } else {
+        Controlador_enviaDadosServer(nomeControlador, ip, porta, senhaApi, "/porta_aberta", "{\"magnetico\":false}");
+      }
+
     }
-  
+
     //VERIFICA SE O SENSOR PIR ESTA CAPTANDO MOVIMENTO
     //NO MEU CASO O zero significa o presença no pir
-    if(estadoSensorPir == 1){
-       Controlador_enviaDadosServer(nomeControlador,ip,porta,senhaApi,"/pir","{\"pir\":true}");  
+    if (estadoSensorPir == 1) {
+      Controlador_enviaDadosServer(nomeControlador, ip, porta, senhaApi, "/pir", "{\"pir\":true}");
     }
     enviar_estado = 0;
     delay(3000);
-  }else{
+  } else {
     enviar_estado++;
-    if(enviar_estado == enviar_estado_max){
-      enviar_estado=0;
+    if (enviar_estado == enviar_estado_max) {
+      enviar_estado = 0;
       Serial.println("Enviando keep-alive de porta");
-      Controlador_enviaDadosServer(nomeControlador,ip,porta,senhaApi,"/porta_aberta","{\"magnetico\":true}");  
+      Controlador_enviaDadosServer(nomeControlador, ip, porta, senhaApi, "/porta_aberta", "{\"magnetico\":"+String(estadoSensorMagnetico)+"}");
     }
-    delay(1000); 
+    delay(1000);
   }
 }
-void fazerReset(){
+void fazerReset() {
   int fazerReset = digitalRead(PINO_RESET);
-  if(fazerReset == 1){
-     //LIGADOR O PINO D0 NO 3V com um botao e ligado 10k no ground
-     EEPROM_limpaEEPROM();
-     delay(100);
-     EEPROM_gravaNovaStringEEPROM("#A#Venizao#venizao123#testesmart.ddns.net#10000#teste#0");
-     delay(100);
-     Serial.println("Resetando configs do controlador.");
-     Controlador_resetaControlador();
+  if (fazerReset == 1) {
+    //LIGADOR O PINO D0 NO 3V com um botao e ligado 10k no ground
+    EEPROM_limpaEEPROM();
+    delay(100);
+    EEPROM_gravaNovaStringEEPROM("#A#Venizao#venizao123#testesmart.ddns.net#10000#teste#0");
+    delay(100);
+    Serial.println("Resetando configs do controlador.");
+    Controlador_resetaControlador();
   }
 }
 
